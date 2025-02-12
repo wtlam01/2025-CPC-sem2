47166c0dcb0bbd74554cf0249c03fa509bf9db57
WL <146025629+wtlam01@users.noreply.github.com>
2025-02-12 16:36:58 +0000

    .

---
+++ Assets/Scripts/Interactions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+   public void selected()
+   {
+        Debug.Log("ship selected");
+   }
+}
