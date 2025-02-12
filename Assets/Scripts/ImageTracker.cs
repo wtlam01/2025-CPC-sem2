3bf0020d7a13513cb9fead6ef49a1fcaf1583608
WL <146025629+wtlam01@users.noreply.github.com>
2025-02-12 15:58:53 +0000

    new scripts

--- Assets/Scripts/ImageTracker.cs
+++ Assets/Scripts/ImageTracker.cs
@@ -6,22 +6,25 @@ using UnityEngine.XR.ARFoundation; // include xr library
 public class TrackImage : MonoBehaviour
 {
     [SerializeField]
-    ARTrackedImageManager m_TrackedImageManager; 
+    ARTrackedImageManager m_TrackedImageManager;
     public GameObject shipPrefab; //Prefab you want to appear on marker image
-    
+
     void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
 
     void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
 
     void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-    	// When the camera picks up a new image marker Unity adds a game object to it called newImage, this will stick to maker.
+        AudioSource source = GetComponent<AudioSource>();
+
+        // When the camera picks up a new image marker Unity adds a game object to it called newImage, this will stick to maker.
         foreach (ARTrackedImage newImage in eventArgs.added)
         {
-        	// Create new copy of your prefab
-        	GameObject newObject = GameObject.Instantiate(shipPrefab);
-        	// parent prefab to the newImage so that they stick together.
-			newObject.transform.SetParent(newImage.transform, false);
+            // Create new copy of your prefab
+            GameObject newObject = GameObject.Instantiate(shipPrefab);
+            // parent prefab to the newImage so that they stick together.
+            newObject.transform.SetParent(newImage.transform, false);
+            source.Play();
         }
     }
 }
